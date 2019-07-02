ed4e8d95e8e7aaf4508d530e6c3946d6d1bb0614
CaptainEFFF <davidbanville4@gmail.com>
2019-07-02 11:29:35 -0400

    please work

--- config/connection.js
+++ config/connection.js
@@ -9,9 +9,9 @@ if(process.env.JAWSDB_URL){
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
+  password: "DRB4oreo",
   database: "burger_db"
-});
+});}
 
 connection.connect(function(err) {
     if (err) {
